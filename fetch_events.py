7f1d5ff520c162ad06d3bde0601270101c408b74
Cole Gleason <colewashere@gmail.com>
2013-04-10 21:39:22 -0500

    Finished visualization and calendar fetching (in Pytohn)

---
+++ fetch_events.py
@@ -0,0 +1,159 @@
+# The following code is heavily adapted from the Google Calendar API Python client example.
+
+import json
+import datetime
+import httplib2
+
+# Parse dates into datetime objects
+from dateutil import parser
+# Write dates out in the correct format according to RFC3339
+from rfc3339 import rfc3339
+
+# Google API
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import run
+
+# google api keys
+client_id = "93879183907.apps.googleusercontent.com"
+client_secret = "rA1h_3sK-j2Co6pmK_i5_C0_"
+
+# These are cole's calendars.  Run print_cals functions to get id's
+# and put them here to get your data.
+enabled_cals = ("primary",
+                "nrqhn9en88ltti9djpi60t98n0@group.calendar.google.com",
+                "dmoun4g5vi6vrk3a76tqn02l9o@group.calendar.google.com")
+
+# The scope URL for read/write access to a user's calendar data
+scope = 'https://www.googleapis.com/auth/calendar'
+
+# Create a flow object. This object holds the client_id, client_secret, and
+# scope. It assists with OAuth 2.0 steps to get user authorization and
+# credentials.
+flow = OAuth2WebServerFlow(client_id, client_secret, scope)
+
+def print_cals(service):
+    """
+    Print out calendar names and their ids. Add the ones you want to enabled cals.
+    """
+    page_token = None
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        if calendar_list['items']:
+            for calendar_list_entry in calendar_list['items']:
+                print calendar_list_entry['summary']
+                print calendar_list_entry['id']
+
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
+
+def filter_events(event_list):
+    """
+    Remove overlapping events from the calendar.
+    """
+    events = sorted(event_list, key=lambda x: x["start"])
+    curr_event = events[0]
+    to_delete = []
+    for e in events[1:]:
+        if e["start"] < curr_event["end"]:
+            to_delete.append(e)
+        else:
+            curr_event = e
+    events = [e for e in events if e not in to_delete]
+    return events
+
+def insert_free_time(event_list):
+    """
+    Insert a 'Free Time' event between all other events in the list.
+    """
+    events = sorted(event_list, key=lambda x: x["start"])
+    free_events = []
+    for i in range(len(events) - 1):
+        if events[i]["end"] != events[i + 1]["start"]:
+            e = {"summary":"Free Time",
+                 "start": events[i]["end"],
+                 "end": events[i + 1]["start"]
+            }
+            free_events.append(e)
+    events += free_events
+    return sorted(events, key=lambda x: x["start"])
+
+
+
+
+def main():
+    # Storage object to hold User credentials between calls.  Only holds one
+    # user at a time.
+    storage = Storage('credentials.dat')
+
+    # The get() function returns the credentials for the Storage object. If no
+    # credentials were found, None is returned.
+    credentials = storage.get()
+
+    # If no credentials are found or the credentials are invalid due to
+    # expiration, new credentials need to be obtained from the authorization
+    # server. The oauth2client.tools.run() function attempts to open an
+    # authorization server page in your default web browser. The server
+    # asks the user to grant your application access to the user's data.
+    # If the user grants access, the run() function returns new credentials.
+    # The new credentials are also stored in the supplied Storage object,
+    # which updates the credentials.dat file.
+    if credentials is None or credentials.invalid:
+        credentials = run(flow, storage)
+
+    # Create an httplib2.Http object to handle our HTTP requests, and authorize it
+    # using the credentials.authorize() function.
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+
+    # The apiclient.discovery.build() function returns an instance of an API service
+    # object can be used to make API calls. The object is constructed with
+    # methods specific to the calendar API. The arguments provided are:
+    #   name of the API ('calendar')
+    #   version of the API you are using ('v3')
+    #   authorized httplib2.Http() object that can be used for API calls
+    service = build('calendar', 'v3', http=http)
+
+    try:
+        event_list = []
+        keys = {"end", "start", "summary"}
+        time_min = rfc3339(datetime.datetime.now() - datetime.timedelta(1))
+        for cal in enabled_cals:
+            request = service.events().list(calendarId=cal,
+                                            singleEvents=True,
+                                            timeMin=time_min)
+            while request != None:
+                # Get the next page.
+                response = request.execute()
+                # Accessing the response like a dict object with an 'items' key
+                # returns a list of item objects (events).
+                for event in response.get('items', []):
+                    if keys.issubset(set(event.keys()))\
+                        and "dateTime" in event["start"].keys()\
+                        and "dateTime" in event["end"].keys():
+                        e = {"summary":event["summary"],
+                             "start": int(parser.parse(event["start"]["dateTime"]).strftime("%s")) - 3600,
+                             "end": int(parser.parse(event["end"]["dateTime"]).strftime("%s")) - 3600
+                        }
+                        event_list.append(e)
+
+                # the list_next method.
+                request = service.events().list_next(request, response)
+        print "total: " + str(len(event_list))
+        event_list = filter_events(event_list)
+        print "filtered: " + str(len(event_list))
+        event_list = insert_free_time(event_list)
+        print "total with free: " + str(len(event_list))
+        with open("event_list.json", "w") as f:
+            json.dump(event_list, f, indent=4, separators=(',', ': '))
+    except AccessTokenRefreshError:
+        # The AccessTokenRefreshError exception is raised if the credentials
+        # have been revoked by the user or they have expired.
+        print ('The credentials have been revoked or expired, please re-run'
+               'the application to re-authorize')
+
+if __name__ == '__main__':
+    main()
--- text_effects/text_effects.pde
+++ text_effects/text_effects.pde
@@ -1,3 +1,5 @@
+import org.json.*;
+
 int [] positions; 
 PFont f;
 float transitionCount;
@@ -6,30 +8,36 @@ Event now;
 Event then;
 Event transition;
 long durationStart;
-long testStart; //for testing, can be removed
+long eventStart; //for testing, can be removed
+int currEvent;
+ArrayList events;
 
 void setup() {
-  size(400, 400);
+  size(600, 400);
   background(0);
   frameRate(30);
   setupPositions();
   setupFont();
   thenTransition = (positions[1] - positions[0])/255.0;
-  //testing! 
-  //must set a now and then to begin, as well as start the duration
-  Event _now = new Event("First Event", 30000000000L); //duration: 1 minute
-  Event _then = new Event("Second Event", 30000000000L);
-  testStart=System.nanoTime(); //for testing, can be removed
-  
+  events = loadEvents("event_list.json");  
+  currEvent = currEventIndex(events);
+  if (currEvent < 0) {
+    println("Could not find the current event!");
+    exit();
+  }
+  Event _now = (Event) events.get(currEvent);
+  Event _then = (Event) events.get(currEvent + 1);
+  eventStart=System.nanoTime(); //for testing, can be removed
+
   initialize(_now, _then);
 }
 
 void draw() {
   background(0);
-  testFunction();
+  checkNewEvent();
   text("Now:", width/8, positions[0]);
   text("Then:", width/8, positions[1]);
-  if(transitionCount > 0.0){
+  if (transitionCount > 0.0) {
     transition();
   }
   else {
@@ -39,28 +47,33 @@ void draw() {
   }
 }
 
-void testFunction(){
-  long testDuration = System.nanoTime() - testStart;
-  if (testDuration >= now.duration){
-    Event newEvent = new Event("Next Event", 60000000000L);
+void checkNewEvent() {
+  long eventDuration = System.nanoTime() - eventStart;
+  if (eventDuration >= now.duration) {
+    currEvent++;
+    if ( currEvent > events.size()) {
+      println("No more events!");
+      exit();
+    }
+    Event newEvent = (Event) events.get(currEvent);
     pushEvent(newEvent);
-    testStart = System.nanoTime();
+    eventStart = System.nanoTime();
   }
 }
 
 void initialize(Event _now, Event _then) {
   now = _now;
   then = _then;
-  durationStart = System.nanoTime();
+  durationStart = _now.start * 1000000000L;
 }
 
-void pushEvent(Event _transition){
+void pushEvent(Event _transition) {
   transition = _transition;
   durationStart = System.nanoTime();
   transitionCount = 255;
 }
 
-void drawDurationCircles(){
+void drawDurationCircles() {
   float startX = width/8 + 10.0;
   float endX = rightmostText() - 10.0;
   float spaceBetween = (endX-startX)/7;
@@ -69,12 +82,12 @@ void drawDurationCircles(){
   }
   float timeEllapsed = (float)(System.nanoTime() - durationStart);
   float percentEllapsed = timeEllapsed/now.duration;
-  for(int i = 0; i < 8; i++){
+  for (int i = 0; i < 8; i++) {
     if (percentEllapsed >i/8.0 && percentEllapsed <(i+1)/8.0)
     {
       fill(255 - 255*(8.0*(percentEllapsed - (i/8.0))));
     }
-    else if (percentEllapsed > i/8.0){
+    else if (percentEllapsed > i/8.0) {
       fill(0);
     }
     ellipse(startX + spaceBetween*i, height/2, 20, 20);
@@ -82,7 +95,7 @@ void drawDurationCircles(){
   }
 }
 
-void transition(){
+void transition() {
   fadeText(transitionCount, 3*width/8, positions[0], now.name);
   transitionText();
   fadeText(255-transitionCount, 3*width/8, positions[1], transition.name);
@@ -94,43 +107,81 @@ void transition(){
   }
 }
 
-void fadeText(float fade, int x, int y, String t){
-  fill(255,fade);
+void fadeText(float fade, int x, int y, String t) {
+  fill(255, fade);
   text(t, x, y);
   fill(255);
 }
 
-void transitionText(){
+void transitionText() {
   float yPosition = positions[1] - (thenTransition * (255-transitionCount));
   text(then.name, 3*width/8, yPosition);
 }
 
-void setupPositions(){
+void setupPositions() {
   positions = new int[2];
   positions[0] = 3*height/8;
   positions[1] = 5*height/8;
 }
 
-void setupFont(){
+void setupFont() {
   f = createFont("Arial", 36, true);
   fill(255);
   textFont(f, 36);
   textAlign(LEFT, CENTER);
 }
 
-float rightmostText(){
+float rightmostText() {
   float nowTW = textWidth(now.name);
   float thenTW = textWidth(then.name);
   return max(nowTW, thenTW) + (3*width/8);
 }
 
+ArrayList loadEvents(String filename) {
+  JSONArray eventList = new JSONArray();
+  ArrayList events = new ArrayList();
+
+  try {
+    eventList = new JSONArray(join(loadStrings(filename), ""));
+    for (int i = 0; i < eventList.length(); i++) {
+      JSONObject e = eventList.getJSONObject(i);
+      String name = e.getString("summary");
+      int start =  e.getInt("start");
+      int end = e.getInt("end");
+      Event event = new Event(name, start, end);
+      events.add(event);
+    }
+  } 
+  catch(JSONException e) {
+    e.getCause();
+  }    
+
+  return events;
+}
+
+int currEventIndex(ArrayList events) {
+  long time = System.nanoTime() / 1000000000L;
+  println(time);
+   for(int i = 0; i < events.size(); i++) {
+     Event e = (Event) events.get(i);
+     if (time >= e.start && time < e.end) {
+       return i;
+     }
+   } 
+   return -1;
+}
+
 class Event {
   String name;
+  int start;
+  int end;
   long duration;
-  
-  public Event(String _name, long _duration){
+
+  public Event(String _name, int _start, int _end) {
     name = _name;
-    duration = _duration;
+    start = _start;
+    end = _end;
+    duration = (long) pow(10, 9) * (end - start);
   }
-  
 }
+
